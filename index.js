bb3a24be4b9c3356b84b161e0f990ae4c7f2f603
devJake <devjake@outlook.com>
2019-05-06 18:45:47 -0600

    tweaking of boilerplate

--- index.js
+++ index.js
@@ -2,19 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.get root
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-    
-})
-
-//app.listen
-app.listen(3000, function() {
-    console.log("Server started on port 3000");
-
+app.listen(port, () => {
+  console.log(`listenting on port ${port}`);
 });
