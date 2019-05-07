75151237c1b1597048ba396dd6ddbefa46fc7679
devJake <devjake@outlook.com>
2019-05-06 18:57:20 -0600

    fixing paths

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.render(
+      'index',
+      {
+        title: 'Snake-Feed App'
+      }
+  );
 });
 
 app.listen(port, () => {
