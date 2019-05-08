d249469b31dcf594556c2a1e9798c33d02f69c7d
devJake <devjake@outlook.com>
2019-05-08 16:17:49 -0600

    getting info back on the post finally............

--- app.js
+++ app.js
@@ -6,17 +6,42 @@ const debug = require('debug')('app');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-const signinRouter = require('./src/routes/signinRoutes')('Danger Noodle Noms');
-const authRouter = require('./src/routes/authRoutes')('Danger Noodle Noms');
+//const signinRouter = require('./src/routes/signinRoutes')('Danger Noodle Noms');
+//const authRouter = require('./src/routes/authRoutes')('Danger Noodle Noms');
 
-app.use('/signin', signinRouter);
-app.use('/auth', authRouter);
+//app.use('/signin', signinRouter);
+//app.use('/auth', authRouter);
+
+app.post('/auth/signin', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.get('/auth', (req, res) => {
+  res.render(
+    'signin',
+    {
+      title: 'Danger'
+    }
+  )
+})
+
+// authRouter.route('/signin')
+//     .post((req, res) => {
+//       console.log(req.get('content-type'));
+//       console.log(req.headers);
+//       console.log(req.params);
+//       console.log(req.statusMessage);
+//       //debug(req.body);
+//       //console.log(req.body);
+//       res.json(req.body);
+//     });
 
 app.get('/', (req, res) => {
   res.render(
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,16 +5,26 @@ const debug = require('debug')('app:authRoutes');
 const authRouter = express.Router();
 
 function router(titleThing) {
-  authRouter.route('/signin')
-    .post((req, res) => {
-      console.log(req.get('content-type'));
-      console.log(req.headers);
-      console.log(req.params);
-      console.log(req.statusMessage);
-      //debug(req.body);
-      //console.log(req.body);
-      res.json(req.body);
-    });
+  // authRouter.route('/')
+  //   .get((req, res) => {
+  //     res.render(
+  //       'signin',
+  //       {
+  //         title: titleThing
+  //       }
+  //     )
+  //   });
+  // authRouter.route('/signin')
+  //   .post((req, res) => {
+  //     console.log(req.body);
+  //     console.log(req.get('content-type'));
+  //     console.log(req.headers);
+  //     console.log(req.params);
+  //     console.log(req.statusMessage);
+  //     //debug(req.body);
+  //     //console.log(req.body);
+  //     res.json(req.body);
+  //   });
   return authRouter;
 }
 
