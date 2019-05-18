ab898d19673330e6bc9bec1868e31c757279e0bc
devJake <devjake@outlook.com>
2019-05-18 08:44:43 -0600

    installed passport, cookie-parser, and express-session.

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const debug = require('debug')('app');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app = express();
 const port = process.env.PORT || 3000;
